03379fbc7cc6eeabcb875e0684868a5ad6afd0c5
Rahul <143754020+DaRahul02@users.noreply.github.com>
2023-10-18 14:43:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/client'))
 
-const port = process.env.PORT || 3000
+
 app.get('/test', function(request, response) {
 	response.type('text/plain')
 	response.send('Node.js and Express running on port='+port)
@@ -17,8 +17,8 @@ app.get('/api/favorite-places', (req, res) => {
   res.json(favoritePlaces);
 });
 
-
-// start the server
-app.listen(port, function() {
-	console.log("Server is running at http://localhost:3000/")
-})
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
